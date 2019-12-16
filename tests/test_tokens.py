54bb421ae2bef1170945b7bf924a5fa7e24e404b
Kazuya Takei <attakei@gmail.com>
2019-12-17 02:02:02 +0900

    Add case

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -10,10 +10,20 @@ class TokenizeOnlyReader(Reader):
         return super().get_transforms() + [Tokenize]
 
 
-@pytest.mark.parametrize("source,tokens_size", [("本日は晴天なり", 4), ("毎日が日曜日", 3),])
+@pytest.mark.parametrize("source,tokens_size", [("本日は晴天なり", 4), ("毎日が日曜日", 3)])
 def test_single_text_tokenize(source, tokens_size):
     doctree = publish_doctree(source, reader=TokenizeOnlyReader())
     paragraph: nodes.paragraph = doctree.children[0]
     assert "tokens" in paragraph.attributes
     tokens = paragraph.attributes["tokens"]
     assert len(tokens) == tokens_size
+
+
+def test_tokenize_url_reference():
+    source = "`ここ <http://example.com>`_"
+    doctree = publish_doctree(source, reader=TokenizeOnlyReader())
+    paragraph: nodes.paragraph = doctree.children[0]
+    assert "tokens" in paragraph.attributes
+    tokens = paragraph.attributes["tokens"]
+    assert len(tokens) == 1
+    assert tokens[0].surface == "ここ"
