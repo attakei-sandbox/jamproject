de1847ed2d8cc8b47723cb3fc1e4cce5ee5c2923
Kazuya Takei <attakei@gmail.com>
2019-12-18 00:54:03 +0900

    Initialize docs

---
+++ docs/conf.py
@@ -0,0 +1,22 @@
+"""Configuration for documents.
+
+This directory is premise to work on poetry environment of project.
+"""
+import jamproject
+
+
+# Project information
+project = "jamproject"
+copyright = "2019, Kazuya Takei"
+author = "Kazuya Takei"
+release = jamproject.__version__
+
+# General configuration
+extensions = []
+templates_path = ["_templates"]
+language = "ja"
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# Options for HTML output
+html_theme = "alabaster"
+html_static_path = ["_static"]
