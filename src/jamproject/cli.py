b7f5d3416df613c9271551e4c5203bf59c25e80f
Kazuya Takei <attakei@gmail.com>
2019-12-18 18:32:28 +0900

    CLI command collect target sources by argument

--- src/jamproject/cli.py
+++ src/jamproject/cli.py
@@ -1,4 +1,7 @@
 import click
+from pprint import pprint
+from pathlib import Path
+from typing import List, Optional
 from . import __version__
 
 
@@ -9,13 +12,34 @@ def display_version():
     click.echo(f"{package_name} {__version__}")
 
 
+def collect_all_sources(targets: List[str], extensions: List[str], basepath: Optional[Path]=None) -> List[Path]:
+    """Collect file sources.
+
+    :param targes: Collect target resources(include files and directories)
+    :param extensions: List of target extension (used fo directory)
+    :return: Matched files
+    """
+    basepath = Path.cwd() if basepath is None else basepath
+    sources = []
+    for t in targets:
+        target = basepath / t
+        if target.is_file():
+            sources += [target, ]
+            continue
+        sources += [p for p in target.glob("**/*") if p.suffix in extensions]
+    return sources
+
+
 @click.command()
 @click.option("--version", is_flag=True)
-def cmd(version):
+@click.option("--verbose", "-v", is_flag=True,)
+@click.argument("targets", nargs=-1, type=click.Path(exists=True))
+def cmd(version, verbose, targets):
     if version:
         display_version()
         return
-    click.echo("Hello")
+    target_files = collect_all_sources(targets, [".rst"], Path.cwd())
+    pprint(target_files)
 
 
 def main():
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,22 @@
-from jamproject.cli import display_version
+import pytest
+from pathlib import Path
+from jamproject.cli import collect_all_sources, display_version
+
+
+here = Path(__file__).parent.resolve()
 
 
 def test_version(capsys):
     display_version()
     captured = capsys.readouterr()
     assert captured.out == "jamproject 0.1.0\n"
+
+
+@pytest.mark.parametrize("targets,ext,files", [
+    (["./testdata/collect_all_sources"], [".rst"], 1),
+    (["./testdata/collect_all_sources"], [".rst", ".txt"], 2),
+    (["./testdata/collect_all_sources/index.rst"], [], 1),
+])
+def test_collect_all_sources(targets, ext, files):
+    sources = collect_all_sources(targets, ext, here)
+    assert len(sources) == files
