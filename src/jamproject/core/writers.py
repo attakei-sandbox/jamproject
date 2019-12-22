5c2346105ef4f040ba182dc05e9f45d984013fc5
Kazuya Takei <attakei@gmail.com>
2019-12-22 17:35:06 +0900

    Refactor name and docs

--- src/jamproject/core/writers.py
+++ src/jamproject/core/writers.py
@@ -3,33 +3,42 @@ from docutils.writers import Writer as BaseWriter
 
 
 class ReportTranslator(NodeVisitor):
+    """Reporting formatter translations
+    """
+
     def __init__(self, document):
         super().__init__(document)
         self.body = []
 
-    def visit_any(self, node):
+    def _visit_any(self, node):
+        """Register reports and pointer( filename + lineno) into output body.
+
+        This is called when visited any node.
+        """
         if "report" not in node:
             return
         report = node["report"]
         if len(report) <= 0:
             return
-        self.body.append([
-            f"{node.source}:{node.line}-",
-            [m.body for m in report._messages],
-        ])
+        self.body.append(
+            [f"{node.source}:{node.line}-", [m.body for m in report._messages],]
+        )
 
-    def pass_node(self, node):
+    def _depart_any(self, node):
         pass
 
     def __getattr__(self, name):
         if name.startswith("visit_"):
-            return self.visit_any
+            return self._visit_any
         if name.startswith("depart_"):
-            return self.pass_node
+            return self._depart_any
         return super().__getattr__(name)
 
 
 class Writer(BaseWriter):
+    """Custom writer to use ReportTranslator.
+    """
+
     def translate(self):
         translator = ReportTranslator(self.document)
         self.document.walkabout(translator)
