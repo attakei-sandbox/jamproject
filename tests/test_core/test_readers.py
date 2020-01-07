2582e6ee63294b6015f7e282bbd07d6f29da5dbc
Kazuya Takei <attakei@gmail.com>
2020-01-07 18:20:34 +0900

    Add test case

--- tests/test_core/test_readers.py
+++ tests/test_core/test_readers.py
@@ -16,6 +16,15 @@ def test_single_text_tokenize(source, tokens_size):
     assert len(sentences[0]) == tokens_size
 
 
+@pytest.mark.parametrize("source,sentence_num", [("本日は晴天なり。毎日が日曜日", 2)])
+def test_multiple_text_tokenize(source, sentence_num):
+    doctree = publish_doctree(source, reader=Reader())
+    paragraph: nodes.paragraph = doctree.children[0]
+    assert "sentences" in paragraph.attributes
+    sentences = paragraph.attributes["sentences"]
+    assert len(sentences) == sentence_num
+
+
 def test_tokenize_url_reference():
     source = "`ここ <http://example.com>`_"
     doctree = publish_doctree(source, reader=Reader())
