0b7982f057f04de9fd207abd73bd9a42914b55ae
Kazuya Takei <myself@attakei.net>
2020-05-06 02:53:21 +0900

    Delegate collect file path object

--- src/motto/cli.py
+++ src/motto/cli.py
@@ -4,6 +4,7 @@ from click_default_group import DefaultGroup
 from docutils.core import publish_file
 from . import __version__
 from .core.config import load_config, load_default_config
+from .core.path import collect_files, resolve_path
 from .core.readers import Reader
 from .core.writers import Writer
 from .core.loaders import load_skills
@@ -28,6 +29,7 @@ def version():
 def run(targets):
     """Lint files.
     """
+    targets = tuple(resolve_path(t) for t in targets)
     config_path: Path = Path.cwd() / "setup.cfg"
     if config_path.exists():
         config = load_config(config_path)
@@ -37,15 +39,7 @@ def run(targets):
     for skill in load_skills(config):
         reader.add_skill(skill)
     # Collect files
-    collected = []
-    for target in targets:
-        t = Path(target)
-        if t.is_file():
-            collected.append(target)
-            continue
-        # TODO: lint other extension files?
-        for f in t.glob("**/*.rst"):
-            collected.append(f)
+    collected = collect_files(targets, "rst")
     # Lint all files
     for target in collected:
         publish_file(source_path=target, reader=reader, writer=Writer())
